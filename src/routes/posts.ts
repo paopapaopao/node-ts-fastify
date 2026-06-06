0b150730181dab86b0bfd984bc5c9e71f9ebd733
paopapaopao <paolo.casugay@gmail.com>
2026-06-06 19:30:33 +0800

    Add /posts/:id route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,10 +1,23 @@
-import { type FastifyInstance } from 'fastify';
+import { eq } from 'drizzle-orm';
+import { type FastifyInstance, type FastifyRequest } from 'fastify';
 
 import { postsTable } from '../schemas';
 
 export const postsRoutes = (app: FastifyInstance) => {
   app.get('/posts', async (_, __) => {
     const response = await app.db.select().from(postsTable);
+
+    return response;
+  });
+
+  app.get('/posts/:id', async (request: FastifyRequest, __) => {
+    const { id } = request.params as { id: string };
+
+    const [response] = await app.db
+      .select()
+      .from(postsTable)
+      .where(eq(postsTable.id, Number(id)))
+      .limit(1);
 
     return response;
   });
